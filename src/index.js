42e1b3d44bff30b5dbcba767d5d175967ab0f833
Lyakhnovich German <lyakhnovich.german@gmail.com>
2021-04-19 16:13:34 +0300

    webpack test

--- src/index.js
+++ src/index.js
@@ -1,82 +1,18 @@
-var CONTACTS = [
-  {
-    id: 1,
-    name: 'Darth Vader',
-    phoneNumber: '+250966666666',
-    image: 'http://cs7.pikabu.ru/images/big_size_comm_an/2014-03_7/13962622876915.gif'
-  }, {
-    id: 2,
-    name: 'Princess Leia',
-    phoneNumber: '+250966344466',
-    image: 'http://images6.fanpop.com/image/photos/33100000/CARRIE-FISHER-anakin-vader-and-princess-leia-33186069-190-149.gif'
-  }, {
-    id: 3,
-    name: 'Luke Skywalker',
-    phoneNumber: '+250976654433',
-    image: 'https://media1.giphy.com/media/b3tPrlRIgDQOc/200w.gif'
-  }, {
-    id: 4,
-    name: 'Chewbacca',
-    phoneNumber: '+250456784935',
-    image: 'https://media.giphy.com/media/RUUdVZqwpfTRS/giphy.gif'
-  }
-];
+import _ from 'lodash';
+import printMe from './print.js';
 
-var Contact = React.createClass({
-  render: function() {
-    return (
-      <li className="contact">
-        <img className="contact-image" src={this.props.image} width="60px" height="60px" />
-        <div className="contact-info">
-          <div className="contact-name"> {this.props.name} </div>
-          <div className="contact-number"> {this.props.phoneNumber} </div>
-        </div>
-      </li>
-    );
-  }
-});
+function component() {
+  const element = document.createElement('div');
+  const btn = document.createElement('button');
 
-var ContactsList = React.createClass({
-  getInitialState: function() {
-    return {
-      displayedContacts: CONTACTS
-    };
-  },
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
-  handleSearch: function(event) {
-    var searchQuery = event.target.value.toLowerCase();
-    var displayedContacts = CONTACTS.filter(function(el) {
-      var searchValue = el.name.toLowerCase();
-      return searchValue.indexOf(searchQuery) !== -1;
-    });
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
 
-    this.setState({
-      displayedContacts: displayedContacts
-    });
-  },
+  element.appendChild(btn);
 
-  render: function() {
-    return (
-      <div className="contacts">
-        <input type="text" className="search-field" onChange={this.handleSearch} />
-        <ul className="contacts-list">
-          {
-            this.state.displayedContacts.map(function(el) {
-              return <Contact
-                key={el.id}
-                name={el.name}
-                phoneNumber={el.phoneNumber}
-                image={el.image}
-              />;
-            })
-          }
-        </ul>
-      </div>
-    );
-  }
-});
+  return element;
+}
 
-ReactDOM.render(
-  <ContactsList />,
-  document.getElementById("content")
-);
+document.body.appendChild(component());
