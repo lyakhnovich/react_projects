d63f7ed12badfe9e5a6d12e7fa4a902137cf5212
Lyakhnovich German <lyakhnovich.german@gmail.com>
2021-05-21 11:29:48 +0300

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,11 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
   output: {
-    path: path.join(__dirname, "/dist"),
+    path: path.join(__dirname, "./dist"),
     filename: "index-bundle.js"
   },
   devtool: 'inline-source-map',
@@ -29,6 +30,12 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/index.html"
-    })
+    }),
+    new CopyPlugin({
+      patterns: [
+        {from: "./src/styles/style.css", to: "styles/style.css"},
+        {from: "./src/styles/style.css.map", to: "styles/style.css.map"},
+      ],
+    }),
   ]
 };
